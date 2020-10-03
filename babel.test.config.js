71250aa122af92f9d1cfe0a8174ff492ebfba95d
PeckZeg <peck.zeg@gmail.com>
2020-10-03 09:19:16 +0000

    Deploy PeckZeg/vc-motion to github.com/PeckZeg/vc-motion.git:gh-pages

--- babel.test.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('@ayase/ayase-build/lib/getBabelConfig').default({
-  target: 'browser',
-  type: 'cjs',
-  typescript: true,
-  runtimeHelpers: true
-}).opts;
--- src/DomWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'DomWrapper',
-
-  render() {
-    return this.$slots.default();
-  }
-});
